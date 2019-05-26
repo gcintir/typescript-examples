35c7e78873874dbdfbea903b2fa47db514f31b81
gurayci <guray.cintir@argela.com.tr>
2019-05-26 13:32:07 +0300

    initial commit

---
+++ source/Variables.ts
@@ -0,0 +1,11 @@
+/**
+ * Typescript data types
+ * Any: super type of all data types
+ * Built-in types: number, string, boolean, void, null, undefined
+ * User-defined types: Arrays, Enums, Classes, Interfaces
+ */
+
+var country:string = 'Turkey';
+console.log(country);
+
+
